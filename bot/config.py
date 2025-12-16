1f64ffd0f9ad5824a18f9b930a80874ca0986ee8
Omen <152046775+Biskfw@users.noreply.github.com>
2025-12-17 00:10:28 +0700

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -1,11 +1,8 @@
-from pathlib import Path
 import yaml
 
 class Config:
     def __init__(self):
-        config_path = Path("configuration.yaml")
-        with open(config_path, "r") as f:
-            self.data = yaml.safe_load(f)
+        with open("configuration.yaml", "r") as f:
+            data = yaml.safe_load(f)
 
-    def get(self, key, default=None):
-        return self.data.get(key, default)
+        self.bot_token = data["bot_token"]
